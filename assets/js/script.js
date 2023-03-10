79192e76fdb130c5f80cc923f340802f0ee4270d
Raffaela <raffaela.designer@gmail.com>
2023-03-10 22:24:20 +0000

    add code to division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,6 +43,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivideQuestion(num1, num2);
     }
       else {
         alert("Unknown game type: ${gameType}")
@@ -85,7 +87,9 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];        
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    }   
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
+    }  
       else {
         alert("Unimplemented operator ${operator}");
         throw "Unimplemented operator ${operator}. Aborting!";        
@@ -131,6 +135,8 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 }
 
-function displayDivideQuestion() {
-
+function displayDivideQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "/";
 }
